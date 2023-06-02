784853c32cfca4cd238f56f565c487b7a5a478ce
sorax <robert@hausgedacht.de>
2023-06-02 17:57:59 +0200

    add deps: test watch

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Game.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
