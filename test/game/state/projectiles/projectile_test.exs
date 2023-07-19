9b4da6246666788da9a7426a15f7e322a7e3e083
sorax <robert@hausgedacht.de>
2023-07-19 19:14:35 +0200

    update tests

--- test/game/state/projectiles/projectile_test.exs
+++ test/game/state/projectiles/projectile_test.exs
@@ -16,4 +16,44 @@ defmodule Game.State.Projectiles.ProjectileTest do
       assert %Projectile{speed: _, range: _} = projectile
     end
   end
+
+  describe "Projectile tick" do
+    test "decrease projectile range" do
+      uuid = UUID.generate()
+      projectile1 = %Projectile{id: uuid, player_id: @player_id, x_vector: 10, y_vector: 20}
+      projectile2 = Projectile.tick(projectile1)
+
+      assert projectile1.range > projectile2.range
+    end
+
+    test "don't update projectile position when vector is 0,0" do
+      uuid = UUID.generate()
+
+      projectile =
+        %Projectile{id: uuid, player_id: @player_id, x_vector: 0, y_vector: 0}
+        |> Projectile.tick()
+
+      assert %Projectile{x: 0, y: 0} = projectile
+    end
+
+    test "don't update projectile position when speed is 0" do
+      uuid = UUID.generate()
+
+      projectile =
+        %Projectile{id: uuid, player_id: @player_id, x_vector: 10, y_vector: 10, speed: 0}
+        |> Projectile.tick()
+
+      assert %Projectile{x: 0, y: 0} = projectile
+    end
+
+    test "update position when speed and vector are not 0" do
+      uuid = UUID.generate()
+
+      projectile =
+        %Projectile{id: uuid, player_id: @player_id, x_vector: 10, y_vector: 10}
+        |> Projectile.tick()
+
+      refute match?(%Projectile{x: 0, y: 0}, projectile)
+    end
+  end
 end
--- test/game/state/projectiles_test.exs
+++ test/game/state/projectiles_test.exs
@@ -4,8 +4,7 @@ defmodule Game.State.ProjectilesTest do
   alias Game.State.Projectiles
   alias Game.State.Projectiles.Projectile
 
-  @player_id1 "random-id-1"
-  @player_id2 "random-id-2"
+  @player_id "random-id"
 
   describe "projectiles Agent is automatically started" do
     test "has a pid and is running" do
@@ -19,59 +18,31 @@ defmodule Game.State.ProjectilesTest do
 
   describe "projectiles state" do
     test "can list projectiles" do
-      Projectiles.add(@player_id1, {0, 0}, {0, 0})
+      Projectiles.add(@player_id, {0, 0}, {0, 0})
 
       projectiles_list = Projectiles.list()
 
       assert is_list(projectiles_list)
-      assert projectiles_list |> Enum.find(&(&1.player_id === @player_id1))
+      assert projectiles_list |> Enum.find(&(&1.player_id === @player_id))
     end
 
     test "add projectile returns added projectile" do
-      projectile = Projectiles.add(@player_id1, {0, 0}, {10, 10})
+      projectile = Projectiles.add(@player_id, {0, 0}, {10, 10})
 
-      assert %Projectile{player_id: @player_id1, x_vector: 10, y_vector: 10} = projectile
+      assert %Projectile{player_id: @player_id, x_vector: 10, y_vector: 10} = projectile
     end
   end
 
   describe "projectiles tick" do
-    test "decrease projectile range" do
-      %{range: start_range} = Projectiles.add(@player_id1, {0, 0}, {10, 10})
-
-      Projectiles.tick()
-
-      assert %Projectile{range: end_range} =
-               Projectiles.list() |> Enum.find(&(&1.player_id === @player_id1))
-
-      assert end_range < start_range
-    end
-
     test "remove projectiles with no range left" do
-      %{range: range} = Projectiles.add(@player_id1, {0, 0}, {10, 10})
+      %{range: range} = Projectiles.add(@player_id, {0, 0}, {10, 10})
 
-      assert Projectiles.list() |> Enum.find(&(&1.player_id === @player_id1))
+      assert Projectiles.list() |> Enum.find(&(&1.player_id === @player_id))
 
       ticks = range + 1
       Enum.each(1..ticks, fn _x -> Projectiles.tick() end)
 
-      refute Projectiles.list() |> Enum.find(&(&1.player_id === @player_id1))
-    end
-
-    test "update projectile positions" do
-      Projectiles.add(@player_id1, {0, 0}, {0, 0})
-      Projectiles.add(@player_id2, {0, 0}, {10, 10})
-
-      Projectiles.tick()
-
-      projectiles_list = Projectiles.list()
-
-      assert %Projectile{x: 0, y: 0} =
-               Enum.find(projectiles_list, &(&1.player_id === @player_id1))
-
-      refute match?(
-               %Projectile{x: 0, y: 0},
-               Enum.find(projectiles_list, &(&1.player_id === @player_id2))
-             )
+      refute Projectiles.list() |> Enum.find(&(&1.player_id === @player_id))
     end
   end
 end
