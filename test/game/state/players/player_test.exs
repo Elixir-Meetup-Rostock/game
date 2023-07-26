4e13094de0f3e86c32f97bcc017fac75bae8eb12
scrusi <scrusi@gmail.com>
2023-07-26 14:50:53 +0200

    fix test

--- test/game/state/players/player_test.exs
+++ test/game/state/players/player_test.exs
@@ -41,7 +41,7 @@ defmodule Game.State.Players.PlayerTest do
     test "no position update on tick, if no action is true" do
       player = %Player{id: @id, name: @name}
 
-      assert %{x: 0, y: 0} = Player.tick(player)
+      assert %{x: 0, y: 0} = Player.tick(player, [])
     end
 
     test "no position update on tick, if opposite action is true" do
@@ -50,8 +50,8 @@ defmodule Game.State.Players.PlayerTest do
       player_y = player |> Player.set_action(:up, true) |> Player.set_action(:down, true)
       player_x = player |> Player.set_action(:left, true) |> Player.set_action(:right, true)
 
-      assert %{x: 0, y: 0} = Player.tick(player_y)
-      assert %{x: 0, y: 0} = Player.tick(player_x)
+      assert %{x: 0, y: 0} = Player.tick(player_y, [])
+      assert %{x: 0, y: 0} = Player.tick(player_x, [])
     end
 
     test "update position on tick, when action is true" do
@@ -59,10 +59,10 @@ defmodule Game.State.Players.PlayerTest do
       s_pl = player.speed
       s_mi = -player.speed
 
-      assert %{x: 0, y: ^s_mi} = player |> Player.set_action(:up, true) |> Player.tick()
-      assert %{x: ^s_mi, y: 0} = player |> Player.set_action(:left, true) |> Player.tick()
-      assert %{x: ^s_pl, y: 0} = player |> Player.set_action(:right, true) |> Player.tick()
-      assert %{x: 0, y: ^s_pl} = player |> Player.set_action(:down, true) |> Player.tick()
+      assert %{x: 0, y: ^s_mi} = player |> Player.set_action(:up, true) |> Player.tick([])
+      assert %{x: ^s_mi, y: 0} = player |> Player.set_action(:left, true) |> Player.tick([])
+      assert %{x: ^s_pl, y: 0} = player |> Player.set_action(:right, true) |> Player.tick([])
+      assert %{x: 0, y: ^s_pl} = player |> Player.set_action(:down, true) |> Player.tick([])
     end
   end
 end
