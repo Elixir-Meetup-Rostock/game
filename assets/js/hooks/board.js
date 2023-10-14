c1730726b766fcdeaa9175a0087fdc687378b1d9
sorax <robert@hausgedacht.de>
2023-10-14 22:14:13 +0200

    animate tiles that have frames

--- assets/js/hooks/board.js
+++ assets/js/hooks/board.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // import Projectile from "./board/projectile"
-// import Tile from "./board/tile"
+import Tile from "./board/tile"
 
 export default class Board {
   constructor(node, sprites, layers, player) {
@@ -13,6 +13,8 @@ export default class Board {
     this.context = this.canvas.getContext("2d")
 
     this.sprites = sprites
+
+    this.tiles = {}
 
     this.layers = layers
     this.player = player
@@ -82,6 +84,10 @@ export default class Board {
       window.cancelAnimationFrame(this.animationFrameId)
     }
 
+    for (let id in this.tiles) {
+      this.tiles[id].tick()
+    }
+
     this.animationFrameId = window.requestAnimationFrame(() => {
       this.drawFrame()
       this.drawFps()
@@ -105,8 +111,12 @@ export default class Board {
       const xPos = this.halfWidth - this.player.x + x
       const yPos = this.halfHeight - this.player.y + y
 
+      if (!this.tiles[id]) {
+        this.tiles[id] = new Tile(this.sprites[sprite], size, frames)
+      }
+
       // TODO: add this.zoom to the drawing
-      this.context.drawImage(this.sprites[sprite], 0, 0, size, size, xPos, yPos, size, size)
+      this.context.drawImage(this.tiles[id].canvas, 0, 0, size, size, xPos, yPos, size, size)
     })
   }
 
@@ -114,8 +124,11 @@ export default class Board {
     const xPos = this.halfWidth - (size / 2)
     const yPos = this.halfHeight - (size / 2)
 
+    if (!this.tiles[id]) {
+      this.tiles[id] = new Tile(this.sprites[sprite], size, frames)
+    }
 
     // TODO: add this.zoom to the drawing
-    this.context.drawImage(this.sprites[sprite], 0, 0, size, size, xPos, yPos, size, size)
+    this.context.drawImage(this.tiles[id].canvas, 0, 0, size, size, xPos, yPos, size, size)
   }
 }
--- assets/js/hooks/board/tile.js
+++ assets/js/hooks/board/tile.js
@@ -1,8 +1,11 @@
 "use strict";
 
 export default class Tile {
-  constructor(sprite, size) {
+  constructor(sprite, size, frames = 0, frame = 0) {
     this.sprite = sprite
+    this.size = size
+    this.frames = frames
+    this.frame = frame
 
     this.canvas = document.createElement("canvas")
     this.canvas.width = size
@@ -14,8 +17,17 @@ export default class Tile {
     // this.context.restore()
     // this.context.translate(this.canvas.width / 2, this.canvas.height / 2)
 
-    this.context.drawImage(sprite, 0, 0, size, size, 0, 0, size, size)
+    this.drawFrame()
+  }
 
-    return this.canvas
+  tick() {
+    if (this.frames > 0) {
+      this.frame = (this.frame + 1) % this.frames
+      this.drawFrame()
+    }
+  }
+
+  drawFrame() {
+    this.context.drawImage(this.sprite, this.size * this.frame, 0, this.size, this.size, 0, 0, this.size, this.size)
   }
 }
