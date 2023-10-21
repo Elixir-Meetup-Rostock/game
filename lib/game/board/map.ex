9b29d698e54cecc373270b10d926a13a4e5b89d9
sorax <robert@hausgedacht.de>
2023-10-21 17:52:05 +0200

    store game-map in json file

--- lib/game/board/map.ex
+++ lib/game/board/map.ex
@@ -3,57 +3,34 @@ defmodule Game.Board.Map do
   Holds the data of the map
   """
 
+  @dir Path.expand("../../../priv/map", __DIR__)
+
   def get() do
-    %{
-      {0, 0} => "rock",
-      {16, 0} => "rock",
-      {32, 0} => "rock",
-      {48, 0} => "rock",
-      {64, 0} => "rock",
-      {80, 0} => "rock",
-      {96, 0} => "rock",
-      {0, 16} => "rock",
-      {16, 16} => "grass",
-      {32, 16} => "grass",
-      {48, 16} => "grass",
-      {64, 16} => "grass",
-      {80, 16} => "grass",
-      {96, 16} => "rock",
-      {0, 32} => "rock",
-      {16, 32} => "grass",
-      {32, 32} => "grass",
-      {48, 32} => "grass_flowers",
-      {64, 32} => "grass",
-      {80, 32} => "grass",
-      {96, 32} => "rock",
-      {0, 48} => "rock",
-      {16, 48} => "grass_flowers",
-      {32, 48} => "grass",
-      {48, 48} => "grass",
-      {64, 48} => "grass",
-      {80, 48} => "grass",
-      {96, 48} => "rock",
-      {0, 64} => "rock",
-      {16, 64} => "grass",
-      {32, 64} => "grass",
-      {48, 64} => "grass",
-      {64, 64} => "grass_flowers",
-      {80, 64} => "grass",
-      {96, 64} => "rock",
-      {0, 80} => "rock",
-      {16, 80} => "grass",
-      {32, 80} => "grass",
-      {48, 80} => "grass",
-      {64, 80} => "grass",
-      {80, 80} => "grass",
-      {96, 80} => "rock",
-      {0, 96} => "rock",
-      {16, 96} => "rock",
-      {32, 96} => "rock",
-      {48, 96} => "rock",
-      {64, 96} => "rock",
-      {80, 96} => "rock",
-      {96, 96} => "rock"
-    }
+    load()
+    |> Enum.map(fn %{"x" => x, "y" => y, "sprite" => sprite} -> {{x, y}, sprite} end)
+    |> Map.new()
   end
+
+  # def set(map) do
+  #   body =
+  #     get()
+  #     |> Enum.map(fn {{x, y}, sprite} -> %{x: x, y: y, sprite: sprite} end)
+
+  #   store(body)
+  # end
+
+  defp load() do
+    [@dir, "default.json"]
+    |> Path.join()
+    |> File.read!()
+    |> Jason.decode!()
+  end
+
+  # defp store(map) do
+  #   body = Jason.encode!(map)
+
+  #   [@dir, "default.json"]
+  #   |> Path.join()
+  #   |> File.write(body)
+  # end
 end
