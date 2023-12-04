934379d52b5fdd7138a972c868408eb7914c318b
NoahLeu <noah.leu99@gmail.com>
2023-12-04 20:39:14 +0100

    fixed migration for previous users without username!

--- priv/repo/migrations/20231016181014_user_account_update.exs
+++ priv/repo/migrations/20231016181014_user_account_update.exs
@@ -1,12 +1,26 @@
 defmodule Game.Repo.Migrations.UserAccountUpdate do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:users) do
-      # add unique username
-      add :username, :string, null: false
+      add :username, :string
+    end
+
+    flush()
+    Game.Repo.update_all("users", set: [username: "user#{System.unique_integer()}"])
+
+    alter table(:users) do
+      modify :username, :string, null: false
     end
 
     create unique_index(:users, [:username])
+  end
+
+  def down do
+    drop index(:users, [:username])
+
+    alter table(:users) do
+      remove :username
+    end
   end
 end
