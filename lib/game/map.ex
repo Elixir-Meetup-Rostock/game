6b74b6d672c0c667c51f2cf871c0cd69128c9ce5
Z0tt3l <z0tt3ls@gmail.com>
2023-12-11 20:08:07 +0100

    fly.io init

--- lib/game/map.ex
+++ lib/game/map.ex
@@ -21,8 +21,7 @@ defmodule Game.Map do
   def load(file, name \\ __MODULE__) do
     {sprites, tiles, spawns, obstacles} =
       [@dir, file]
-      |> Path.join()
-      |> File.read!()
+      |> get_default_map(true)
       |> Jason.decode!()
       |> create_structs()
 
@@ -81,5 +80,413 @@ defmodule Game.Map do
 
   defp create_obstacle(attrs) do
     %Obstacle{} |> Obstacle.changeset(attrs) |> Obstacle.to_struct()
+  end
+
+  defp get_default_map(file_path, false) do
+    file_path
+    |> Path.join()
+    |> File.read!()
+  end
+
+  defp get_default_map(_file_path, true) do
+    """
+        {
+          "sprites": [
+            {
+              "id": "player_blue",
+              "src": "/images/sprites/character_idle_blue.png",
+              "frames": 4,
+              "obstacle": true
+            },
+            {
+              "id": "player_green",
+              "src": "/images/sprites/character_idle_green.png",
+              "frames": 4,
+              "obstacle": true
+            },
+            {
+              "id": "player_blue_walking",
+              "src": "/images/sprites/character_walking_blue.png",
+              "frames": 4,
+              "obstacle": true
+            },
+            {
+              "id": "player_green_walking",
+              "src": "/images/sprites/character_walking_green.png",
+              "frames": 4,
+              "obstacle": true
+            },
+            {
+              "id": "demo_tree",
+              "src": "/images/sprites/demo_tree.png",
+              "obstacle": true
+            },
+            {
+              "id": "grass_1",
+              "src": "/images/sprites/grass_1.png"
+            },
+            {
+              "id": "grass_2",
+              "src": "/images/sprites/grass_2.png"
+            },
+            {
+              "id": "grass_flowers",
+              "src": "/images/sprites/grass_flowers.png"
+            },
+            {
+              "id": "grass",
+              "src": "/images/sprites/grass.png"
+            },
+            {
+              "id": "rock",
+              "src": "/images/sprites/rock.png",
+              "obstacle": true
+            },
+            {
+              "id": "tree_stump_left",
+              "src": "/images/sprites/tree_stump_left.png",
+              "obstacle": true
+            },
+            {
+              "id": "tree_stump_right",
+              "src": "/images/sprites/tree_stump_right.png",
+              "obstacle": true
+            },
+            {
+              "id": "water_1",
+              "src": "/images/sprites/water_1.png",
+              "obstacle": true
+            },
+            {
+              "id": "water_2",
+              "src": "/images/sprites/water_2.png",
+              "obstacle": true
+            },
+            {
+              "id": "projectile",
+              "src": "/images/sprites/projectile.png",
+              "obstacle": true
+            }
+          ],
+          "tiles": [
+            {
+              "sprite_id": "grass",
+              "x": 80,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 80,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 32,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 64,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 16,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 32,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 48,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 16,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 64,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass_flowers",
+              "x": 16,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 32,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 80,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 80,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass_flowers",
+              "x": 48,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 48,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 80,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 48,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 16,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 48,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 32,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 48,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 16,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 64,
+              "y": 96,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 32,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 64,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 32,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 48,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 64,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 16,
+              "y": 0,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 64,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 80,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 16,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 48,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 32,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 0,
+              "y": 80,
+              "z": 0
+            },
+            {
+              "sprite_id": "water_1",
+              "x": 96,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "sprite_id": "rock",
+              "x": 64,
+              "y": 64,
+              "z": 0
+            },
+            {
+              "sprite_id": "grass",
+              "x": 80,
+              "y": 32,
+              "z": 0
+            }
+          ],
+          "spawns": [
+            {
+              "x": 16,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "x": 32,
+              "y": 16,
+              "z": 0
+            },
+            {
+              "x": 16,
+              "y": 32,
+              "z": 0
+            },
+            {
+              "x": 32,
+              "y": 32,
+              "z": 0
+            }
+          ]
+        }
+    """
   end
 end
